b7c1cf2f60272f3198877c46aa5eb4dd6ee99d21
mikey- <mikey-@users.noreply.github.com>
2022-04-22 10:44:57 +1000

    Create config.fish
    For some reason there's a conda init for fish now :P

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval ${HOME}/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
