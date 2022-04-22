e023f0d850fd9c3cec4b3f077db56be29191f309
mikey- <mikey-@users.noreply.github.com>
2022-04-22 10:54:18 +1000

    Update conda.sh
    Use anaconda paths in conda script

--- .script.d/conda.sh
+++ .script.d/conda.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-__conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$("${HOME}/opt/anaconda3/bin/conda" 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "${HOME}/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/opt/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="${HOME}/miniconda3/bin:$PATH"
+        export PATH="${HOME}/opt/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 
-conda activate mikey;
+conda activate base;
