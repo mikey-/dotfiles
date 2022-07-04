e72c4aa4d2e29ff74ab902bd5d0530f24fe53719
mikey <mikeydevops@gmail.com>
2022-07-04 11:23:52 +1000

    Portability: specific profile name -> variable

--- .function.d/mail.sh
+++ .function.d/mail.sh
@@ -13,13 +13,13 @@ function mail {
   );
 
   declare -A MAIL_ACTIONS=(\
-   [ls]="aws s3 ls --profile default-personal-admin --recursive --human-readable --summarize ${ACCOUNT_REMOTE_MAPPING[$2]}/${2}"\
-   [sync]="aws s3 sync --profile default-personal-admin --quiet ${ACCOUNT_REMOTE_MAPPING[$2]} ${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}" \
+   [ls]="aws s3 ls --profile ${AWS_DEFAULT_PROFILE} --recursive --human-readable --summarize ${ACCOUNT_REMOTE_MAPPING[$2]}/${2}"\
+   [sync]="aws s3 sync --profile ${AWS_DEFAULT_PROFILE} --quiet ${ACCOUNT_REMOTE_MAPPING[$2]} ${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}" \
   );
 
   [[ ! -d "${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}" ]] || mkdir -p "${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}"
 
   ${MAIL_ACTIONS[${1}]};
 
-  /usr/local/bin/tree -axCFNQ --noreport --sort=mtime "${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}";
+  tree --sort=mtime "${MAIL_STORAGE_LOCAL}/${2}@${MAIL_DOMAIN}";
 }
